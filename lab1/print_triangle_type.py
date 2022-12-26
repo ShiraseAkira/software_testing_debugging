47106b237ba3924be380ae8e6224c54ed194de4d
Maxim Laptev <justsaynya@gmail.com>
2022-12-26 20:09:50 +0300

    fix: rename var sides to triangle_sides

--- lab1/print_triangle_type.py
+++ lab1/print_triangle_type.py
@@ -7,44 +7,44 @@ OK_EXIT_CODE = 0
 NOT_TRIANGLE_MESSAGE = 'не треугольник'
 EQUILATERAL_TRIANLGE_MESSAGE = 'равносторонний'
 
-def get_triangle_sides(sides_lengths):
-    if len(sides_lengths) != 3:
+def get_triangle_triangle_sides(triangle_sides_lengths):
+    if len(triangle_sides_lengths) != 3:
         print(ERROR_MESSAGE)
         sys.exit(ERROR_EXIT_CODE)
     try:
-        sides = [float(sides_lengths[0]), float(sides_lengths[1]), float(sides_lengths[2])]
+        triangle_sides = [float(triangle_sides_lengths[0]), float(triangle_sides_lengths[1]), float(triangle_sides_lengths[2])]
     except ValueError:
         print(ERROR_MESSAGE)
         sys.exit(ERROR_EXIT_CODE)
-    return sides
+    return triangle_sides
 
-def is_sides_valid(sides):
-    for side in sides:
+def is_triangle_sides_valid(triangle_sides):
+    for side in triangle_sides:
         if side <= 0:
             return False
     return True
 
-def is_triangle(sides):
-    return  sides[0] + sides[1] > sides[2] and \
-            sides[1] + sides[2] > sides[0] and \
-            sides[2] + sides[0] > sides[1] 
+def is_triangle(triangle_sides):
+    return  triangle_sides[0] + triangle_sides[1] > triangle_sides[2] and \
+            triangle_sides[1] + triangle_sides[2] > triangle_sides[0] and \
+            triangle_sides[2] + triangle_sides[0] > triangle_sides[1] 
 
-def is_equilateral(sides):
-    return sides[0] == sides[1] and sides[0] == sides[2]
+def is_equilateral(triangle_sides):
+    return triangle_sides[0] == triangle_sides[1] and triangle_sides[0] == triangle_sides[2]
 
 
-def get_triangle_type(sides_lengths):
-    sides = get_triangle_sides(sides_lengths)
+def get_triangle_type(triangle_sides_lengths):
+    triangle_sides = get_triangle_triangle_sides(triangle_sides_lengths)
 
-    if not is_sides_valid(sides):
+    if not is_triangle_sides_valid(triangle_sides):
         print(NOT_TRIANGLE_MESSAGE)
         sys.exit(OK_EXIT_CODE)
 
-    if not is_triangle(sides):
+    if not is_triangle(triangle_sides):
         print(NOT_TRIANGLE_MESSAGE)
         sys.exit(OK_EXIT_CODE)
 
-    if is_equilateral(sides):
+    if is_equilateral(triangle_sides):
         print(EQUILATERAL_TRIANLGE_MESSAGE)
 
     print('OK')
