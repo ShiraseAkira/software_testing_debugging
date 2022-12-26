aa94f511e821330aa0a14fd4587b9d47ad0aca05
Maxim Laptev <justsaynya@gmail.com>
2022-12-26 20:18:13 +0300

    feat: add check and tests for isoscele triange, fix: remove excessive test cases

--- lab1/print_triangle_type.py
+++ lab1/print_triangle_type.py
@@ -6,8 +6,9 @@ OK_EXIT_CODE = 0
 
 NOT_TRIANGLE_MESSAGE = 'не треугольник'
 EQUILATERAL_TRIANLGE_MESSAGE = 'равносторонний'
+ISOSCELES_TRIANGLE_MESSAGE = 'равнобедренный'
 
-def get_triangle_triangle_sides(triangle_sides_lengths):
+def get_triangle_sides(triangle_sides_lengths):
     if len(triangle_sides_lengths) != 3:
         print(ERROR_MESSAGE)
         sys.exit(ERROR_EXIT_CODE)
@@ -32,9 +33,14 @@ def is_triangle(triangle_sides):
 def is_equilateral(triangle_sides):
     return triangle_sides[0] == triangle_sides[1] and triangle_sides[0] == triangle_sides[2]
 
+def is_isosceles(triangle_sides):
+    return  (triangle_sides[0] == triangle_sides[1] and triangle_sides[0] != triangle_sides[2]) or \
+            (triangle_sides[1] == triangle_sides[2] and triangle_sides[1] != triangle_sides[0]) or \
+            (triangle_sides[2] == triangle_sides[0] and triangle_sides[2] != triangle_sides[1])
+
 
 def get_triangle_type(triangle_sides_lengths):
-    triangle_sides = get_triangle_triangle_sides(triangle_sides_lengths)
+    triangle_sides = get_triangle_sides(triangle_sides_lengths)
 
     if not is_triangle_sides_valid(triangle_sides):
         print(NOT_TRIANGLE_MESSAGE)
@@ -46,6 +52,8 @@ def get_triangle_type(triangle_sides_lengths):
 
     if is_equilateral(triangle_sides):
         print(EQUILATERAL_TRIANLGE_MESSAGE)
+    elif is_isosceles(triangle_sides):
+        print(ISOSCELES_TRIANGLE_MESSAGE)
 
     print('OK')
 
